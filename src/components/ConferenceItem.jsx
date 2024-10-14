59029f24ca3108f618024ed42b53b023bdb9e524
degnonepithech <josue.allagbe@epitech.eu>
2024-10-14 04:29:37 +0100

    [WIP] - Select by date of conference

--- src/components/ConferenceItem.jsx
+++ src/components/ConferenceItem.jsx
@@ -1,35 +1,94 @@
+import PropTypes from "prop-types";
 
-import PropTypes from "prop-types"
-
-
-
-const ConferenceItem = ({ cover, name, price, text }) => {
-    return (
-      <div className="max-w-sm rounded overflow-hidden shadow-lg h-[345px]">
-        <img className="w-full" src={cover} alt="Card image cap" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{name}</div>
-          <p className="text-gray-700 text-base">{text}</p>
-        </div>
-        {price && (
-          <div className="px-6 pt-4 pb-2">
-            <span className="text-gray-500">{price}</span>
-          </div>
-        )}
+const ConferenceItem = ({ cover, title }) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg h-[370px]">
+      {cover && <img className="w-full" src={cover} alt="Card image cap" />}
+      <div className="px-6 py-4">
+        <div className="font-bold /**text-xl**/ mb-2">Titre : {title}</div>
+        {/* <p className="text-gray-700 text-base">Intervenant : {speaker}</p>
+        <p className="text-gray-700 text-base">Heure : {time}</p>
+        <p className="text-gray-700 text-base">Date : {date}</p> */}
       </div>
-    );
-  };
+    </div>
+  );
+};
+
+// Ajout des propTypes pour valider les props
+ConferenceItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  speaker: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
+
+export default ConferenceItem;
+
+
+/////////////////
+
+// import PropTypes from "prop-types";
+
+// const ConferenceItem = ({ cover, title, speaker, time, date }) => {
+//   return (
+//     <div className="max-w-sm rounded overflow-hidden shadow-lg h-[370px]">
+//       {cover && <img className="w-full" src={cover} alt="Card image cap" />}
+//       <div className="px-6 py-4">
+//         <div className="font-bold /**text-xl**/ mb-2">Titre : {title}</div>
+//         <p className="text-gray-700 text-base">Intervenant : {speaker}</p>
+//         <p className="text-gray-700 text-base">Heure : {time}</p>
+//         <p className="text-gray-700 text-base">Date : {date}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// // Ajout des propTypes pour valider les props
+// ConferenceItem.propTypes = {
+//   cover: PropTypes.string,
+//   title: PropTypes.string.isRequired,
+//   speaker: PropTypes.string.isRequired,
+//   time: PropTypes.string.isRequired,
+//   date: PropTypes.string.isRequired,
+// };
+
+// export default ConferenceItem;
+
+
+///////////
+
+// import PropTypes from "prop-types"
+
+
+
+// const ConferenceItem = ({ cover, name, price, text }) => {
+//     return (
+//       <div className="max-w-sm rounded overflow-hidden shadow-lg h-[345px]">
+//         <img className="w-full" src={cover} alt="Card image cap" />
+//         <div className="px-6 py-4">
+//           <div className="font-bold text-xl mb-2">{name}</div>
+//           <p className="text-gray-700 text-base">{text}</p>
+//         </div>
+//         {price && (
+//           <div className="px-6 pt-4 pb-2">
+//             <span className="text-gray-500">{price}</span>
+//           </div>
+//         )}
+//       </div>
+//     );
+//   };
   
-  // Ajout des propTypes pour valider les props
-  ConferenceItem.propTypes = {
-    cover: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.string,
-    text: PropTypes.string.isRequired,
-  }
+//   // Ajout des propTypes pour valider les props
+//   ConferenceItem.propTypes = {
+//     cover: PropTypes.string.isRequired,
+//     name: PropTypes.string.isRequired,
+//     price: PropTypes.string,
+//     text: PropTypes.string.isRequired,
+//   }
 
 
-export default ConferenceItem
+// export default ConferenceItem
 
 
 
--- src/components/ConferencesList.jsx
+++ src/components/ConferencesList.jsx
@@ -5,7 +5,7 @@ import Categories from './Categories'
 import PropTypes from "prop-types"
 
 
-function ConferencesList({ cart, updateCart }) {
+function ConferencesList() {
 	const [activeCategory, setActiveCategory] = useState('')
 	const categories = plantList.reduce(
 		(acc, elem) =>
@@ -13,20 +13,6 @@ function ConferencesList({ cart, updateCart }) {
 		[]
 	)
 
-	function addToCart(name, price) {
-		const currentPlantAdded = cart.find((plant) => plant.name === name)
-		if (currentPlantAdded) {
-			const cartFilteredCurrentPlant = cart.filter(
-				(plant) => plant.name !== name
-			)
-			updateCart([
-				...cartFilteredCurrentPlant,
-				{ name, price, amount: currentPlantAdded.amount + 1 }
-			])
-		} else {
-			updateCart([...cart, { name, price, amount: 1 }])
-		}
-	}
 
 	return (
 		<div className='w-full'>
@@ -37,17 +23,16 @@ function ConferencesList({ cart, updateCart }) {
 			/>
 
 			<div className='list-none p-8 flex flex-wrap justify-center gap-5 '>
-				{plantList.map(({ id, cover, name, water, light, price, category }) =>
+				{plantList.map(({ id, cover, title, water, light, price, category }) =>
 					!activeCategory || activeCategory === category ? (
 						<div key={id}>
 							<ConferenceItem
 								cover={cover}
-								name={name}
+								title={title}
 								water={water}
 								light={light}
 								price={price}
 							/>
-							<button onClick={() => addToCart(name, price)}>Ajouter</button>
 						</div>
 					) : null
 				)}
@@ -63,3 +48,72 @@ ConferencesList.propTypes = {
   }
 
 export default ConferencesList
+
+
+//////////////
+
+// import { useState } from 'react'
+// import { plantList } from '../constants/plantList'
+// import ConferenceItem from './ConferenceItem'
+// import Categories from './Categories'
+// import PropTypes from "prop-types"
+
+
+// function ConferencesList() {
+// 	const [activeCategory, setActiveCategory] = useState('')
+// 	const categories = plantList.reduce(
+// 		(acc, elem) =>
+// 			acc.includes(elem.category) ? acc : acc.concat(elem.category),
+// 		[]
+// 	)
+
+// 	// function addToCart(name, price) {
+// 	// 	const currentPlantAdded = cart.find((plant) => plant.name === name)
+// 	// 	if (currentPlantAdded) {
+// 	// 		const cartFilteredCurrentPlant = cart.filter(
+// 	// 			(plant) => plant.name !== name
+// 	// 		)
+// 	// 		updateCart([
+// 	// 			...cartFilteredCurrentPlant,
+// 	// 			{ name, price, amount: currentPlantAdded.amount + 1 }
+// 	// 		])
+// 	// 	} else {
+// 	// 		updateCart([...cart, { name, price, amount: 1 }])
+// 	// 	}
+// 	// }
+
+// 	return (
+// 		<div className='w-full'>
+// 			<Categories
+// 				categories={categories}
+// 				setActiveCategory={setActiveCategory}
+// 				activeCategory={activeCategory}
+// 			/>
+
+// 			<div className='list-none p-8 flex flex-wrap justify-center gap-5 '>
+// 				{plantList.map(({ id, cover, title, water, light, price, category }) =>
+// 					!activeCategory || activeCategory === category ? (
+// 						<div key={id}>
+// 							<ConferenceItem
+// 								cover={cover}
+// 								title={title}
+// 								water={water}
+// 								light={light}
+// 								price={price}
+// 							/>
+// 							{/* <button onClick={() => addToCart(name, price)}>Ajouter</button> */}
+// 						</div>
+// 					) : null
+// 				)}
+// 			</div>
+// 		</div>
+// 	)
+// }
+
+// // Ajout des propTypes pour valider les props
+// ConferencesList.propTypes = {
+//     cart: PropTypes.string.isRequired,
+//     updateCart: PropTypes.string.isRequired,
+//   }
+
+// export default ConferencesList
