320ab97beac9aca1cf22c3fdb4473e5937867b35
degnonepithech <josue.allagbe@epitech.eu>
2024-10-28 21:01:35 +0100

    Update Sposors

--- src/components/Partenaires.jsx
+++ src/components/Partenaires.jsx
@@ -69,501 +69,156 @@ const Partenaires = () => {
       </div>
 
       <div className="flex justify-between items-center mb-5">
-        <img 
-          src={loloAndoche} alt="Lolo Andoche" 
-          className="w-1/4 sm:w-1/6 " />
         
         <img 
           src={ofmas} alt="OFMAS" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={bangeBankBenin} alt="Sadinter" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={lnb} alt="Agoualand" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
+
+<img 
+          src={chaftel} alt="Lolo Andoche" 
+          className="w-1/5 sm:w-1/6 " />
+
+<img 
+          src={sobebra} alt="OFMAS" 
+          className="w-1/5 sm:w-1/6 " />
         
       </div>
 
       <div className="flex justify-between items-center mb-5">
-        <img 
-          src={chaftel} alt="Lolo Andoche" 
-          className="w-1/4 sm:w-1/6 " />
-        
-        <img 
-          src={sobebra} alt="OFMAS" 
-          className="w-1/4 sm:w-1/6 " />
         
         <img 
           src={cidSuperDecor} alt="Sadinter" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={laTour} alt="Agoualand" 
-          className="w-1/4 sm:w-1/6 " />
-        
-      </div>
+          className="w-1/5 sm:w-1/6 " />
 
-      <div className="flex justify-between items-center mb-5">
-        <img 
+<img 
           src={mensah} alt="Lolo Andoche" 
-          className="w-1/4 sm:w-1/6 " />
-        
-        <img 
+          className="w-1/5 sm:w-1/6 " />
+
+<img 
           src={nsia} alt="OFMAS" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={sanlam} alt="Sadinter" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
+        
+      </div>
+
+      <div className="flex justify-between items-center mb-5">
         
         <img 
           src={lab} alt="Agoualand" 
-          className="w-1/4 sm:w-1/6 " />
-        
-      </div>
+          className="w-1/5 sm:w-1/6 " />
 
-      <div className="flex justify-between items-center mb-5">
-        <img 
+<img 
           src={erevan} alt="Lolo Andoche" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={bouclier} alt="OFMAS" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={cpts} alt="Sadinter" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={laRoche} alt="Agoualand" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
       </div>
 
       <div className="flex justify-between items-center mb-5">
-        <img 
+
+<img 
           src={batimat} alt="Lolo Andoche" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={celtiis} alt="OFMAS" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={moov} alt="Sadinter" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={mtn} alt="Agoualand" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
+
+<img 
+          src={aoa} alt="Lolo Andoche" 
+          className="w-1/5 sm:w-1/6 " />
         
       </div>
 
       <div className="flex justify-between items-center mb-5">
-        <img 
+
+<img 
           src={aoa} alt="Lolo Andoche" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={sonibank} alt="OFMAS" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={nobila} alt="Sadinter" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
         
         <img 
           src={atlantiqueAssurances} alt="Agoualand" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/5 sm:w-1/6 " />
+        
+        <img 
+          src={sunu} alt="Lolo Andoche" 
+          className="w-1/5 sm:w-1/6 " />
         
       </div>
 
       <div className="flex justify-between items-center mb-5">
-        <img 
-          src={sunu} alt="Lolo Andoche" 
-          className="w-1/4 sm:w-1/6 " />
-        
-        <img 
+
+<img 
           src={abrp} alt="OFMAS" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/6 sm:w-1/6 " />
         
         <img 
           src={boutique} alt="Sadinter" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/6 sm:w-1/6 " />
         
         <img 
           src={uac} alt="Agoualand" 
-          className="w-1/4 sm:w-1/6 " />
-        
-      </div>
+          className="w-1/6 sm:w-1/6 " />
 
-      <div className="flex justify-between items-center mb-5">
-        <img 
+<img 
           src={ms} alt="Lolo Andoche" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/6 sm:w-1/6 " />
         
         <img 
           src={pharmacie} alt="OFMAS" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/6 sm:w-1/6 " />
 
         <img 
           src={lesTradipraticiens} alt="OFMAS" 
-          className="w-1/4 sm:w-1/6 " />
+          className="w-1/6 sm:w-1/6 " />
         
       </div>
-    </div>
+
+         </div>
   );
 };
 
 export default Partenaires;
-
-
-////////////////////////
-
-// import sonaec from "../assets/partenaires/sonaec.png";
-// import atc from "../assets/partenaires/atc.png";
-// import sadinter from "../assets/partenaires/sadinter.png";
-// import agoualand from "../assets/partenaires/agoualand.png";
-// import loloAndoche from "../assets/partenaires/lolo-andoche.png";
-// import ofmas from "../assets/partenaires/ofmas.png";
-// import bangeBankBenin from "../assets/partenaires/bange-bank-benin.png";
-// import lnb from "../assets/partenaires/lnb.png";
-
-// import chaftel from "../assets/partenaires/chaftel.png";
-// import sobebra from "../assets/partenaires/sobebra.png";
-// import cidSuperDecor from "../assets/partenaires/cid-super-decor.png";
-// import laTour from "../assets/partenaires/la-tour.png";
-
-// import mensah from "../assets/partenaires/mensah.png";
-// import nsia from "../assets/partenaires/nsia.png";
-// import sanlam from "../assets/partenaires/sanlam.png";
-// import lab from "../assets/partenaires/lab.png";
-
-// import erevan from "../assets/partenaires/erevan.png";
-// import bouclier from "../assets/partenaires/bouclier.png";
-// import cpts from "../assets/partenaires/cpts.png";
-// import laRoche from "../assets/partenaires/la-roche.png";
-
-
-// import batimat from "../assets/partenaires/batimat.png";
-// import celtiis from "../assets/partenaires/celtiis.png";
-// import moov from "../assets/partenaires/moov.png";
-// import mtn from "../assets/partenaires/mtn.png";
-
-// import aoa from "../assets/partenaires/aoa.png";
-// import sonibank from "../assets/partenaires/sonibank.png";
-// import nobila from "../assets/partenaires/nobila.png";
-// import atlantiqueAssurances from "../assets/partenaires/atlantique-assurances.png";
-
-// import sunu from "../assets/partenaires/sunu.png";
-// import abrp from "../assets/partenaires/abrp.png";
-// import boutique from "../assets/partenaires/boutique.png";
-// import uac from "../assets/partenaires/uac.png";
-
-// import ms from "../assets/partenaires/ms.png";
-// import pharmacie from "../assets/partenaires/pharmacie.png";
-// import lesTradipraticiens from "../assets/partenaires/les-tradipraticiens.png";
-
-// const Partenaires = () => {
-//   return (
-//     <div className="container mx-auto mt-10 md:p-20 gap-8">
-//       <div className="flex justify-between items-center mb-5">
-//         <img 
-//           src={sonaec} alt="Sonaec" 
-//           className="w-1/4 sm:w-1/6 " /> {/* Ajustement des tailles */}
-        
-//         <img 
-//           src={atc} alt="ATC" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={sadinter} alt="Sadinter" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={agoualand} alt="Agoualand" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//       </div>
-
-//       <div className="flex justify-between items-center mb-5">
-//         <img 
-//           src={loloAndoche} alt="Lolo Andoche" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={ofmas} alt="OFMAS" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={bangeBankBenin} alt="Sadinter" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={lnb} alt="Agoualand" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//       </div>
-
-//       <div className="flex justify-between items-center mb-5">
-//         <img 
-//           src={chaftel} alt="Lolo Andoche" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={sobebra} alt="OFMAS" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={cidSuperDecor} alt="Sadinter" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={laTour} alt="Agoualand" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//       </div>
-
-//       <div className="flex justify-between items-center mb-5">
-//         <img 
-//           src={mensah} alt="Lolo Andoche" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={nsia} alt="OFMAS" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={sanlam} alt="Sadinter" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={lab} alt="Agoualand" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//       </div>
-
-//       <div className="flex justify-between items-center mb-5">
-//         <img 
-//           src={erevan} alt="Lolo Andoche" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={bouclier} alt="OFMAS" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={cpts} alt="Sadinter" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={laRoche} alt="Agoualand" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//       </div>
-
-//       <div className="flex justify-between items-center mb-5">
-//         <img 
-//           src={batimat} alt="Lolo Andoche" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={celtiis} alt="OFMAS" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={moov} alt="Sadinter" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={mtn} alt="Agoualand" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//       </div>
-
-//       <div className="flex justify-between items-center mb-5">
-//         <img 
-//           src={aoa} alt="Lolo Andoche" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={sonibank} alt="OFMAS" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={nobila} alt="Sadinter" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={atlantiqueAssurances} alt="Agoualand" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//       </div>
-
-//       <div className="flex justify-between items-center mb-5">
-//         <img 
-//           src={sunu} alt="Lolo Andoche" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={abrp} alt="OFMAS" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={boutique} alt="Sadinter" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={uac} alt="Agoualand" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//       </div>
-
-//       <div className="flex justify-between items-center mb-5">
-//         <img 
-//           src={ms} alt="Lolo Andoche" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//         <img 
-//           src={pharmacie} alt="OFMAS" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-
-//         <img 
-//           src={lesTradipraticiens} alt="OFMAS" 
-//           className="w-1/4 sm:w-1/6 md:w-1/6" />
-        
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Partenaires;
-
-
-///////////////////////
-// import sonaec from "../assets/partenaires/sonaec.png"
-// import atc from "../assets/partenaires/atc.png"
-// import sadinter from "../assets/partenaires/sadinter.png"
-// import agoualand from "../assets/partenaires/agoualand.png"
-// import loloAndoche from "../assets/partenaires/lolo-andoche.png"
-// import ofmas from "../assets/partenaires/ofmas.png"
-
-
-// const Partenaires = () => {
-
-//   return (
-//     <div className="container mx-auto mt-10">
-//       <div className="flex flex-row justify-between">
-//         <img 
-//           src={sonaec} alt="Kevin Rush" className=" " />
-
-//         <img 
-//           src={atc} alt="Kevin Rush" className=" " />
-
-//         <img 
-//           src={sadinter} alt="Kevin Rush" className=" " />
-
-//         <img 
-//           src={agoualand} alt="Kevin Rush" className=" " />
-
-//         <img 
-//           src={loloAndoche} alt="Kevin Rush" className=" " />
-
-//         <img 
-//           src={ofmas} alt="Kevin Rush" className=" " />
-        
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Partenaires;
-
-
-
-
-///////////////////////
-
-
-// import { motion } from "framer-motion";
-// import agenceRegulationPharmaceutique from "../assets/partenaires/ABMed.png";
-// import boutiqueLogo from "../assets/partenaires/boutiqueLogo.png";
-// import ms1 from "../assets/partenaires/ms1.png";
-// import pharmacieLogo from "../assets/partenaires/pharmacieLogo.png";
-// import uacLogo from "../assets/partenaires/uacLogo.png";
-
-// const images = [
-//   agenceRegulationPharmaceutique,
-//   boutiqueLogo,
-//   ms1,
-//   pharmacieLogo,
-//   uacLogo,
-// ];
-
-// const Partenaires = () => {
-//   const scrollSpeed = 5; // Vitesse du défilement (ajustable)
-
-//   return (
-//     <div className=" pb-20">
-//       <motion.h1
-//         whileInView={{ opacity: 1, y: 0 }}
-//         initial={{ opacity: 0, y: -100 }}
-//         transition={{ duration: 1.5 }}
-//         className="my-20 text-center text-4xl"
-//       >
-//         Partenaires
-//       </motion.h1>
-
-//       <div className="relative w-full overflow-hidden">
-//         {/* Conteneur principal */}
-//         <motion.div
-//           className="flex flex-nowrap items-center justify-center"
-//           animate={{ x: ["100%", "-100%"] }} // Mouvement infini de droite à gauche
-//           transition={{ repeat: Infinity, ease: "linear", duration: scrollSpeed * images.length }}
-//         >
-//           {/* Affichage des images dans le carrousel */}
-//           {images.map((src, index) => (
-//             <motion.div
-//               key={index}
-//               className="flex-shrink-0 p-4"
-//             >
-//               {/* Conteneur de l'image avec arrière-plan vert et dimensions spécifiques */}
-//               <div className="bg-gray-100 rounded-2xl w-[200px] h-[100px] flex items-center justify-center">
-//                 <img
-//                   src={src}
-//                   alt={`Image ${index + 1}`}
-//                   className="w-[230px] h-[130px] object-contain" // Image contenue dans les dimensions
-//                 />
-//               </div>
-//             </motion.div>
-//           ))}
-
-//           {/* Dupliquer les images pour créer un effet de continuité */}
-//           {images.map((src, index) => (
-//             <motion.div
-//               key={`duplicate-${index}`}
-//               className="flex-shrink-0 p-4"
-//             >
-//               {/* Conteneur de l'image avec arrière-plan vert et dimensions spécifiques */}
-//               <div className="bg-gray-100 rounded-2xl w-[200px] h-[100px] flex items-center justify-center">
-//                 <img
-//                   src={src}
-//                   alt={`Duplicate Image ${index + 1}`}
-//                   className="w-[230px] h-[130px] object-contain" // Image contenue dans les dimensions
-//                 />
-//               </div>
-//             </motion.div>
-//           ))}
-//         </motion.div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Partenaires;
-
 
--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,5 +1,3 @@
-// import { Link } from 'react-router-dom';
-
 import Partenaires from "../Partenaires";
 
 
@@ -8,7 +6,8 @@ import Partenaires from "../Partenaires";
 const Sponsors = () => {
   return (
     <div className="container mx-auto py-20">
-        <h2 className="text-customGreen text-center text-4xl font-bold mb-12 underline">NOS SPONSORS</h2>
+        <h2 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl
+        text-customGreen text-center font-bold mb-8 underline">NOS SPONSORS</h2>
         <Partenaires />
     </div>
   );
